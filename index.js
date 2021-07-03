2ed82f0ae81311de6fc0173e5c76aeb85a7b87ee
josevitorvieira <82124025+josevitorvieira@users.noreply.github.com>
2021-07-03 00:29:02 -0300

    Update index.js

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ const api = express();
 const port = process.env.PORT || 3000;
 
 const startApi = ()=>{
+    api.use(cors());
     api.use(express.json());
     api.use('/', employeeController);
-    api.use(cors());
 
     api.listen(port,()=>{
         console.log("Aplicação Iniciada!!!");
